b7d7eb723157ae0c33e18748b34334d50ec73873
Lucas Dias <lucasfdiasdev@gmail.com>
2024-03-02 00:45:07 -0300

    feat: "setup shadcn-ui init"

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const LayoutAuth = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};
+
+export default LayoutAuth;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["files.stripe.com", "subdomain", "utfs.io"],
+  },
+  reactStrictMode: false,
+};
 
 export default nextConfig;
