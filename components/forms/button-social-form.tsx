43317f75eec678aa53169e67aa50a221a1ef6d86
Lucas Dias <lucasfdiasdev@gmail.com>
2024-03-03 01:42:10 -0300

    add: "add onClick providers"

--- components/forms/button-social-form.tsx
+++ components/forms/button-social-form.tsx
@@ -1,15 +1,35 @@
+"use client";
+
+import { signIn } from "next-auth/react";
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 
 import { Button } from "@/components/ui/button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 const ButtonSocialForm = () => {
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+
   return (
     <div className="flex items-center w-full gap-x-2">
-      <Button variant="outline" size="icon" className="w-full">
+      <Button
+        variant="outline"
+        size="icon"
+        className="w-full"
+        onClick={() => onClick("google")}
+      >
         <FcGoogle className="h-5 w-5" />
       </Button>
-      <Button variant="outline" size="icon" className="w-full">
+      <Button
+        variant="outline"
+        size="icon"
+        className="w-full"
+        onClick={() => onClick("github")}
+      >
         <FaGithub className="h-5 w-5" />
       </Button>
     </div>
