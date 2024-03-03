8993ef69da725643ee8fa8e4a7b2fd0d1e35a957
Lucas Dias <lucasfdiasdev@gmail.com>
2024-03-03 20:35:43 -0300

    feat: "create reset actions"

---
+++ actions/reset.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import * as z from "zod";
+
+import { ResetSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
+import { generatePasswordResetToken } from "@/lib/tokens";
+import { sendPasswordResetEmail } from "@/lib/mail";
+
+export const reset = async (values: z.infer<typeof ResetSchema>) => {
+  const validatedFields = ResetSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Email inválido!" };
+  }
+
+  const { email } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser) {
+    return { error: "Email não existe!" };
+  }
+
+  // TODO: Generate token & send email
+  const passwordResetToken = await generatePasswordResetToken(email);
+  await sendPasswordResetEmail(
+    passwordResetToken.email,
+    passwordResetToken.token
+  );
+
+  return { success: "Reset email sent!" };
+};
