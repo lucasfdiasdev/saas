0f7eda936ebe41d961effa5ffbaf40b1bbfe3210
Lucas Dias <lucasfdiasdev@gmail.com>
2024-03-03 19:02:21 -0300

    feat: "create agency page"

--- app/(main)/agency/page.tsx
+++ app/(main)/agency/page.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-const AgencyPage = () => {
+const AgencyPage = async () => {
+  const session = await auth();
+  if (!session) return redirect("/sign-in");
+
+  // get user details
+  // const user = await getAuthUserDetails()
   return <div>AgencyPage</div>;
 };
 
